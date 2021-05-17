010c5bf276c72f5c8baccba9b4ba56a82ef3b17c
nass92 <goujanassim@hotmail.com>
2021-05-17 21:39:10 +0200

    ewe

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Home from './Pages/HomePage/Home'
 import CoinsApi from "./Pages/LsCoins/Api";
 
@@ -8,10 +8,22 @@ function App({ setCrypto }) {
 
   return (
     <Router>
-      <Switch>
-        <Route path='/home' exact component={Home} />
-        <Route path='/list' component={CoinsApi} />
-      </Switch>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/list">ListCoin</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path='/home' exact component={Home} />
+          <Route path='/list' component={CoinsApi} />
+        </Switch>
+      </div>
     </Router >
 
   );
