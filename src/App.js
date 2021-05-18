e7fdc82b039b5f0dffa7bdf2264c63f2cf2ee3b4
nass92 <goujanassim@hotmail.com>
2021-05-18 10:07:58 +0200

    ewe

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react"
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './Pages/HomePage/Home'
 import CoinsApi from "./Pages/LsCoins/Api";
 
